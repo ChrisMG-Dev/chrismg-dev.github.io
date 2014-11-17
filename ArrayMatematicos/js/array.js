d008a27745da92b54123470ff939763313975abe
Christopher Muñoz <chrismgdaw@gmail.com>
2014-11-17 12:20:16 +0100

    ArrayMatematicos añadidos

---
+++ ArrayMatematicos/js/array.js
@@ -0,0 +1,11 @@
+/**
+ Mediante prototype, agrega los métodos sumar, restar, trasponer y multiplicar a la clase ArraysMatematicos.
+ Recuerda que el estado de un array deberían de ser los elementos.
+ Recuerda las restricciones y posibilidades de un Array:
+ Sobre las dimensines de los arrays implicados (unidimensional, bidimensional...)
+ Sobre las longitudes de los arrays implicados (1 en adelante)
+ Sobre los contenidos de los arrays implicados (numéricos)
+ Podemos rellenar un Array con valores aleatorios o directamente desde teclado.
+ Una vez creada la clase, demuestra su funcionamiento en una página bien diseñada.
+ Evita las cajas de texto y hazla lo más dinámica posible.
+*/
