a0b08cd7fc6642706de45b2970745c3fc06bb682
Christopher Muñoz <chrismgdaw@gmail.com>
2014-11-18 18:51:11 +0100

    Mas funcionalidades

--- ArrayMatematicos/js/maquetacionArray.js
+++ ArrayMatematicos/js/maquetacionArray.js
@@ -21,10 +21,35 @@ function modoUnidimensional () {
 	caja.id = "elementos";
 	caja.className = "valorVector";
 
+	var bloqueMin = document.createElement("p");
+	bloqueMin.className = "bloque";
+
+	var labelMin = document.createElement("label");
+	labelMin.innerHTML = "Min: ";
+
+	var cajaMin = document.createElement("input");
+	cajaMin.type = "text";
+	cajaMin.id = "min" + contador_array;
+
+	var bloqueMax = document.createElement("p");
+	bloqueMax.className = "bloque";
+	var labelMax = document.createElement("label");
+	labelMax.innerHTML = "Max: ";
+
+	var cajaMax = document.createElement("input");
+	cajaMax.type = "text";
+	cajaMax.id = "max" + contador_array;
+
 	arrayBox.appendChild(titulo);
 	bloque.appendChild(label);
 	bloque.appendChild(caja);
+	bloqueMin.appendChild(labelMin);
+	bloqueMin.appendChild(cajaMin);
+	bloqueMax.appendChild(labelMax);
+	bloqueMax.appendChild(cajaMax);
 	arrayBox.appendChild(bloque);
+	arrayBox.appendChild(bloqueMin);
+	arrayBox.appendChild(bloqueMax);
 	contenedor.appendChild(arrayBox);
 }
 
@@ -37,33 +62,92 @@ function addVector() {
 	var titulo = document.createElement("h3");
 	titulo.innerHTML = "Vector " + contador_array;
 
-	var bloque = document.createElement("p");
-	bloque.className = "bloque";
-	/*
-	var label = document.createElement("label");
-	label.innerHTML = "Elementos: ";
-	*/
+	var bloqueMin = document.createElement("p");
+	bloqueMin.className = "bloque";
 
-	/*
-	var caja = document.createElement("input");
-	caja.type = "text";
-	caja.id = "elementos" + contador_array;
-	caja.className = "valorVector";
-	*/
+	var labelMin = document.createElement("label");
+	labelMin.innerHTML = "Min: ";
+
+	var cajaMin = document.createElement("input");
+	cajaMin.type = "text";
+	cajaMin.id = "min" + contador_array;
+
+	var bloqueMax = document.createElement("p");
+	bloqueMax.className = "bloque";
+	var labelMax = document.createElement("label");
+	labelMax.innerHTML = "Max: ";
+
+	var cajaMax = document.createElement("input");
+	cajaMax.type = "text";
+	cajaMax.id = "max" + contador_array;
 	arrayBox.appendChild(titulo);
-	//bloque.appendChild(label);
-	//bloque.appendChild(caja);
-	arrayBox.appendChild(bloque);
+	bloqueMin.appendChild(labelMin);
+	bloqueMin.appendChild(cajaMin);
+	bloqueMax.appendChild(labelMax);
+	bloqueMax.appendChild(cajaMax);
+	arrayBox.appendChild(bloqueMin);
+	arrayBox.appendChild(bloqueMax);
 	contenedor.appendChild(arrayBox);	
 }
 
-function sumarVectores () {
-	var inputs = document.getElementsById("elementos");
-	var valor = 0;
-	for (var i = 0; i < inputs.length; i++) {
-		valor = Math.max(parseInt(inputs[i].value), valor);
+function mostrarResultado (resultado) {
+	var contenedor = document.getElementById("contenedor");
+	var arrayBox = document.createElement("div");
+	arrayBox.className = "array";
+
+	var titulo = document.createElement("h3");
+	titulo.innerHTML = "Suma";
+
+	arrayBox.appendChild(titulo);
+	arrayBox.appendChild(document.createTextNode(resultado));
+	contenedor.appendChild(arrayBox);
+
+}
+
+function generarValores () {
+	var elementos = document.getElementById("elementos").value,
+		sumando = [],
+		i,
+		j,
+		min,
+		max,
+		suma;
+
+
+	for (i = 0; i < contador_array; i += 1) {
+		min = parseInt(document.getElementById("min" + (i + 1)).value);
+		max = parseInt(document.getElementById("max" + (i + 1)).value);
+		sumando[i] = [];
+		for (j = 0; j < elementos; j += 1) {
+			sumando[i][j] = Math.round(
+				Math.random() * (max - min) + min
+			);
+		}
 	}
-	console.log(valor);
+
+	var bloque = document.getElementsByClassName("array");
+
+	for (var i = 0; i < bloque.length; i += 1) {
+		var cabecera = document.createElement("h3");
+		titulo = document.createTextNode("Vector " + (i + 1));
+		cabecera.appendChild(titulo);
+		bloque[i].innerHTML = "";
+		bloque[i].appendChild(cabecera);
+		bloque[i].appendChild(document.createTextNode("Valores: " + sumando[i]));
+	}
+
+	document.getElementById("addVector").style.display = "none";
+	document.getElementById("sumVectores").style.display = "none";
+
+	return sumando;
+
+}
+
+function sumarVectores () {
+
+	sumando = generarValores();
+	array = new ArraysMatematicos(sumando, true);
+	mostrarResultado(array.sumar());
 }
 window.onload = function () {
 	document.getElementById("unidimensional").onclick = modoUnidimensional;
