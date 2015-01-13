14207083f98e584bf2f9c0a659182b112220b2ca
Christopher Muñoz <chrismgdaw@gmail.com>
2015-01-13 08:40:56 +0100

    Cambios en carpetas y arreglo del ejercicio 1.5.2

--- JQuery/js/parte1.5.2.js
+++ JQuery/js/parte1.5.2.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$("#mayoria_edad").click(function(evento){
-		if ($("#mayoria_edad").attr("checked")){
+		if ($("#mayoria_edad").prop("checked")){
 			$("#formulariomayores").css("display", "block");
 		}else{
 			$("#formulariomayores").css("display", "none");
