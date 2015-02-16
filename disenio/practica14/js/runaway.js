401db39d84bbb04c49a374c274112262d2ce8320
Christopher Muñoz <chrismgdaw@gmail.com>
2015-02-16 20:59:00 +0100

    runaway añadido

--- disenio/practica14/js/runaway.js
+++ disenio/practica14/js/runaway.js
@@ -1,16 +1,16 @@
 $(function(){
 	jQuery.fn.runaway = function (surface) {
 		var that = this;
-
 		return this.each(function() {
 			$(surface).on("mouseover", function(event) {
 				var distance;
+				var pos = $(that).offset();
 				if (event.pageX > $(window).width() / 2) {
 					distance = -event.pageX / 30;
 					if (event.pageX > $(window).width() / 1.5)
 						distance = -25;
 					$(that).css({
-						transform: "translateX(" + distance + "px) " + "translateY(" +  (0 - event.pageY / 30) + "px)"				
+						transform: "translateX(" + distance + "px) " + "translateY(" +  ((pos.top - event.pageY) / 30) + "px)"				
 					});
 				}
 				else {
@@ -19,7 +19,7 @@ $(function(){
 					else 
 						distance = event.pageX / 30;
 					$(that).css({
-						transform: "translateX(" + distance + "px) " + "translateY(" +  (0 - event.pageY / 30) + "px)"				
+						transform: "translateX(" + distance + "px) " + "translateY(" +  ((pos.top - event.pageY) / 30) + "px)"				
 					});
 				}
 
